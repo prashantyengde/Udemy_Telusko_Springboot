e2b161f672e989f13eefedbd344b44c77d1370b9
prashantyengde <prashantyengde@gmail.com>
2026-04-05 23:37:26 +0530

    Ecommerce Order

---
+++ UdemyProjectEcommerceWithOrders/src/main/java/com/telusko/UdemyProjectEcommerceWithOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyProjectEcommerceWithOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyProjectEcommerceWithOrdersApplication.class, args);
+	}
+
+}
---
+++ UdemyProjectEcommerceWithOrders/src/main/java/com/telusko/springecom/model/OrderItem.java
@@ -0,0 +1,74 @@
+package com.telusko.springecom.model;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+
+    private BigDecimal totalPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Order order;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(BigDecimal totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	
+
+}
